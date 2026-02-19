48af38c94f417fba52ddef037903bceb59c82c36
Pietro Scurti <pietroscurti9@gmail.com>
2026-02-19 15:55:01 +0100

    mileston 4, show completata

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,30 +4,38 @@ const posts = require('../data/posts')
 
 function index(req, res) {
 
-    // pippo.get(); // errore 500 test
-    // res.send({
-    //     total: posts.length,
-    //     posts
-    // });
     const sql = 'SELECT * FROM posts';
     // eseguiamo la query!
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         res.json(results);
+
+        // pippo.get(); // errore 500 test
+        // res.send({
+        //     total: posts.length,
+        //     posts
+        // });
     });
 }
 
 function show(req, res) {
 
-    const id = Number(req.params.id); // 3
+    const id = Number(req.params.id);
 
-    const post = posts.find(post => post.id === id);
+    const sql = 'SELECT * FROM posts WHERE id = ?';
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Post not found' });
+        res.json(results[0]);
+    });
 
-    if (!post) {
-        return res.status(404).send('Post non trovato');
-    }
+    // const post = posts.find(post => post.id === id);
 
-    res.json(post);
+    // if (!post) {
+    //     return res.status(404).send('Post non trovato');
+    // }
+
+    // res.json(post);
 
 
 }
